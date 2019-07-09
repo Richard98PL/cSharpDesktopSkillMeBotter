0188405a9fa7072c3493670c72530f3716131fe1
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2019-07-09 02:11:39 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace skillMeDBL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "North 🠙",
+            "West 🠘",
+            "East 🠚",
+            "South 🠛"});
+            this.comboBox1.Location = new System.Drawing.Point(405, 153);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(80, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(414, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "food [F8]";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.checkBox2.Location = new System.Drawing.Point(12, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "eq repair";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(12, 252);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(126, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 8000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1920x1080",
+            "1600x900",
+            "1440x900",
+            "1366x768"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(80, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(104, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 21);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "test resolution";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(500, 287);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "skillMeDBL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace skillMeDBL
+{
+
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer aTimer;
+
+        String comboChoice = "North 🠙";
+        String comboChoiceRes = "1900x1080";
+        Boolean isFoodEnabled = false;
+        Boolean isReapirEnabled = false;
+
+        public static Process[] processes = Process.GetProcessesByName("DBLClient");
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            
+        }
+
+       
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("START"))
+            {
+                SetTimer();
+                button1.Text = "STOP";
+                timer1.Start();
+            }
+            else
+            {
+                if (aTimer != null) aTimer.Stop();
+                button1.Text = "START";
+                timer1.Stop();
+                progressBar1.Value = 0;
+            }
+                
+
+        }
+
+        [STAThread]
+        public void antyKick()
+        {
+            
+            
+                foreach (Process proc in processes)
+                {
+                    SetForegroundWindow(proc.MainWindowHandle);
+                    Thread.Sleep(100);
+
+                    if (comboChoice.Equals("North 🠙")) antyKickDBL("^{DOWN}", "^{UP}");
+                    else if (comboChoice.Equals("West 🠘")) antyKickDBL(" ^{RIGHT}", "^{LEFT}");
+                    else if (comboChoice.Equals("East 🠚")) antyKickDBL(" ^{LEFT}", "^{RIGHT}");
+                    else if (comboChoice.Equals("South 🠛")) antyKickDBL(" ^{UP}", "^{DOWN}");
+
+                }
+            
+        }
+
+        private void SetTimer()
+        {
+            antyKick();
+            Thread.Sleep(100);
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(800000-300);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void antyKickDBL(String s1, String s2)
+        {
+            SendKeys.SendWait(s1);
+            Thread.Sleep(100);
+            SendKeys.SendWait(s2);
+            Thread.Sleep(100);
+            if (isFoodEnabled) eatFood();
+            if (isReapirEnabled) repairEQ();
+
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            antyKick();
+        }
+
+        private void eatFood()
+        {
+            for(int i=0;i<20;i++) SendKeys.SendWait("{F8}");
+        }
+
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (aTimer != null) aTimer.Stop();
+            button1.Text = "START";
+            timer1.Stop();
+            progressBar1.Value = 0;
+            comboChoice = comboBox1.GetItemText(comboBox1.SelectedItem);
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) isFoodEnabled = true;
+            else isFoodEnabled = false;
+
+            if (aTimer != null) aTimer.Stop();
+            button1.Text = "START";
+            timer1.Stop();
+            progressBar1.Value = 0;
+        }
+
+        public void Clicker(int x, int y)
+        {
+            SetCursorPos(x, y);
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, X,Y, 0, 0);
+
+        }
+        public void repairEQ()
+        {
+            if (comboChoiceRes.Equals("1900x1080"))
+            {
+                Clicker(1750, 146); // left hand
+                Clicker(1830, 147); // right hand
+                Clicker(1750, 183); // belt
+                Clicker(1790, 140); // armor
+                Clicker(1790, 177); // legs
+                Clicker(1790, 215); // boots
+            }else if (comboChoiceRes.Equals("1600x900")){
+                Clicker(1432,148); // left hand
+                Clicker(1506,153); // right hand
+                Clicker(1431,186); // belt
+                Clicker(1466,140); // armor
+                Clicker(1470,172); // legs
+                Clicker(1463,212); // boots
+            }
+            else if (comboChoiceRes.Equals("1440x900")){
+                Clicker(1270,148); // left hand
+                Clicker(1347,150); // right hand
+                Clicker(1272,183); // belt
+                Clicker(1309,140); // armor
+                Clicker(1308,179); // legs
+                Clicker(1309,211); // boots
+            }
+            else if (comboChoiceRes.Equals("1366x768")){
+                Clicker(1200,150); // left hand
+                Clicker(1273,150); // right hand
+                Clicker(1200,183); // belt
+                Clicker(1234,141); // armor
+                Clicker(1234,176); // legs
+                Clicker(1234,215); // boots
+            }
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked) isReapirEnabled = true;
+            else isReapirEnabled = false;
+
+            if (aTimer != null) aTimer.Stop();
+            button1.Text = "START";
+            timer1.Stop();
+            progressBar1.Value = 0;
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+            if (progressBar1.Value == 100)
+            {
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (aTimer != null) aTimer.Stop();
+            button1.Text = "START";
+            timer1.Stop();
+            progressBar1.Value = 0;
+            comboChoiceRes = comboBox2.GetItemText(comboBox2.SelectedItem);
+        }
+
+        public void testResolution()
+        {
+
+            if (comboChoiceRes.Equals("1900x1080"))
+            {
+                Thread.Sleep(1000);
+                Clicker(1750, 146); // left hand
+                Thread.Sleep(1000);
+                Clicker(1830, 147); // right hand
+                Thread.Sleep(1000);
+                Clicker(1750, 183); // belt
+                Thread.Sleep(1000);
+                Clicker(1790, 140); // armor
+                Thread.Sleep(1000);
+                Clicker(1790, 177); // legs
+                Thread.Sleep(1000);
+                Clicker(1790, 215); // boots
+                Thread.Sleep(1000);
+            }
+            else if (comboChoiceRes.Equals("1600x900"))
+            {
+                Thread.Sleep(1000);
+                Clicker(1432, 148); // left hand
+                Thread.Sleep(1000);
+                Clicker(1506, 153); // right hand
+                Thread.Sleep(1000);
+                Clicker(1431, 186); // belt
+                Thread.Sleep(1000);
+                Clicker(1466, 140); // armor
+                Thread.Sleep(1000);
+                Clicker(1470, 172); // legs
+                Thread.Sleep(1000);
+                Clicker(1463, 212); // boots
+                Thread.Sleep(1000);
+            }
+            else if (comboChoiceRes.Equals("1440x900"))
+            {
+                Thread.Sleep(1000);
+                Clicker(1270, 148); // left hand
+                Thread.Sleep(1000);
+                Clicker(1347, 150); // right hand
+                Thread.Sleep(1000);
+                Clicker(1272, 183); // belt
+                Thread.Sleep(1000);
+                Clicker(1309, 140); // armor
+                Thread.Sleep(1000);
+                Clicker(1308, 179); // legs
+                Thread.Sleep(1000);
+                Clicker(1309, 211); // boots
+                Thread.Sleep(1000);
+            }
+            else if (comboChoiceRes.Equals("1366x768"))
+            {
+                Thread.Sleep(1000);
+                Clicker(1200, 150); // left hand
+                Thread.Sleep(1000);
+                Clicker(1273, 150); // right hand
+                Thread.Sleep(1000);
+                Clicker(1200, 183); // belt
+                Thread.Sleep(1000);
+                Clicker(1234, 141); // armor
+                Thread.Sleep(1000);
+                Clicker(1234, 176); // legs
+                Thread.Sleep(1000);
+                Clicker(1234, 215); // boots
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            testResolution();
+        }
+    }
+}
